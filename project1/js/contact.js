a15983b9769ed8ae4439737191415fee543a3ef2
Yuval Ben Haim <98409612+cayuval@users.noreply.github.com>
2022-06-12 16:11:05 +0300

    added project files

---
+++ project1/js/contact.js
@@ -0,0 +1,42 @@
+const form = document.querySelector('form');
+console.log(form);
+const firstName = document.querySelector('.firstName');
+const lastName = document.querySelector('.lastName');
+const email = document.querySelector('.email');
+const freeText = document.getElementById('freeText');
+
+function showError(input,messege){
+    const field = input.parentElement;
+    field.className = 'formControll error'
+    const small = field.querySelector('small')
+    small.innerHTML = messege
+}
+function showSucsses(input){
+    input.style.border = '2px blue solid'
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (firstName.value === '') {
+        showError(firstName,'Enter first name')
+    }else{
+        showSucsses(firstName)
+    }
+    if (lastName.value === '') {
+        showError(lastName,'Enter your last name')
+    }else{
+        showSucsses(lastName)
+    }
+    if (freeText.value === '') {
+        showError(freeText,'Enter your request/feedback')
+    }else{
+        showSucsses(freeText)
+    }
+    if(email.value===''||email.value.indexOf('@')==-1){
+        showError(email,'Enter VALID email')
+    }else{
+        showSucsses(email)
+    }
+
+})
+
