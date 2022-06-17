c0081f104562975d1d2f37f25c5c8daa51f18c43
Yuval Ben Haim <98409612+cayuval@users.noreply.github.com>
2022-06-17 18:58:45 +0300

    17/06 update

--- js/contact.js
+++ js/contact.js
@@ -4,6 +4,10 @@ const firstName = document.querySelector('.firstName');
 const lastName = document.querySelector('.lastName');
 const email = document.querySelector('.email');
 const freeText = document.getElementById('freeText');
+const button = document.querySelector('.submitFile');
+console.log(button);
+
+let validFirstName,validLastName,validEmail,validFreeText
 
 function showError(input,messege){
     const field = input.parentElement;
@@ -22,24 +26,34 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
     if (firstName.value === '') {
         showError(firstName,'Enter first name')
+        validFirstName=false
     }else{
         showSucsses(firstName)
+        validFirstName=true
     }
     if (lastName.value === '') {
         showError(lastName,'Enter last name')
+        validLastName=false
     }else{
         showSucsses(lastName)
+        validLastName=true
     }
     if (freeText.value === '') {
         showError(freeText,'Enter your request/feedback')
+        validFreeText=false
     }else{
         showSucsses(freeText)
+        validFreeText=true
     }
     if(email.value===''||email.value.indexOf('@')==-1){
         showError(email,'Enter VALID email')
+        validEmail=false
     }else{
         showSucsses(email)
+        validEmail=true
     }
-
+    if(validEmail===true&&validFirstName===true&&validLastName===true&&validFreeText==true){
+        alert(`Thank you ${firstName.value} ${lastName.value} for your feedback, we will be in touch soon!`)
+    }
 })
 
