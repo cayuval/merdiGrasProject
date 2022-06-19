481559bbe9f345634ab40ae55b92c9572e9add51
Yuval Ben Haim <98409612+cayuval@users.noreply.github.com>
2022-06-19 11:02:31 +0300

    final update before sending

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ const sponsers = [
     {
         name: `walks on's  Walk-On's Bistreaux and Bar`,
         img: 'img/walkonsBar.png',
-        description: `<p>There’s really no better way to describe the uniqueness of Walk-On’s Sports Bistreaux. We start every dish from scratch and use fresh ingredients to bring our mouthwatering.Cajun cuisine to life in every restaurant. Whether you’re here for dinner with the family, date night, cocktails with the girls, or to watch a game on the big screen, we’re always happy to share our Louisiana culture with you.</p><br><p>1009 Poydras St
-        New Orleans, LA 70112
-        504 - 309 - 6530</p>`
+        description: `<p>There’s really no better way to describe the uniqueness of Walk-On’s Sports Bistreaux. We start every dish from scratch and use fresh ingredients to bring our mouthwatering.Cajun cuisine to life in every restaurant. Whether you’re here for dinner with the family, date night, cocktails with the girls, or to watch a game on the big screen, we’re always happy to share our Louisiana culture with you.</p><br>`
         ,
+        location: `1009 Poydras St
+        New Orleans, LA 70112`,
         hours:`Sun- Thurs: 11am - 11pm<br>
 Fri - Sat: 11am - Midnight`,
 link: 'https://walk-ons.com/'
@@ -15,6 +15,7 @@ link: 'https://walk-ons.com/'
     img: 'img/sheratonhotel.png',
     description: 'Bringing people together is what we’ve done since opening our first Sheraton in 1937. Now we’re deeply rooted in over 400 communities across the globe. We are proud to act as a point of connection for millions of travelers, many just like you. Welcome.'
     ,
+    location:'500 Canal Street, New Orleans, LA 70130',
     hours:`24/7`,
     link: 'https://www.tripadvisor.com/Hotel_Review-g60864-d89130-Reviews-Sheraton_New_Orleans_Hotel-New_Orleans_Louisiana.html'
 },
@@ -23,6 +24,7 @@ link: 'https://walk-ons.com/'
     img: 'img/Ace Hotel.png',
     description: `A stone’s throw from the French Quarter, in the soul of the Deep South.`
     ,
+    location: `600 Carondelet St, New Orleans, Louisiana`,
     hours:`24/7`,
     link: 'https://acehotel.com/new-orleans/'
 },
@@ -31,6 +33,8 @@ link: 'https://walk-ons.com/'
     img: 'img/ritzCarelton.png',
     description: `within a historic beaux arts building in new orleans’ french quarter, a hotel offers luxury with a local touch.`
     ,
+    location:`921 Canal Street
+New Orleans, LA `,
     hours:`24/7`,
     link: 'https://www.ritzcarlton.com/en/hotels/new-orleans'
 },
@@ -40,6 +44,8 @@ link: 'https://walk-ons.com/'
     description: `
     local parking place - under the known company of premium parking`
     ,
+    location:`300 N Peters St, New Orleans, LA 70130
+`,
     hours:`24/7`,
     link: 'https://www.premiumparking.com/P402'
 },
@@ -49,6 +55,9 @@ link: 'https://walk-ons.com/'
     description: `
     When you make a child's potential visible, wonderful things happen.`
     ,
+    location:`City Park New Orleans
+15 Henry Thomas Drive
+New Orleans, LA 70124`,
     hours:`Tues- 
     Satu: 9:30am - 4:30pm<br>
     Sund: 11:30am - 4:30pm`,
@@ -60,8 +69,9 @@ link: 'https://walk-ons.com/'
     description: `
     The World Famous Museum of Death was founded in 1995 to fill the void in death education in the USA.`
     ,
+    location:`227 Dauphine St, New Orleans, LA 70112`,
     hours:`Fri-Thur 10am - 6pm`,
-    link: 'img/museumofdeath.png'
+    link: 'http://www.museumofdeath.net/'
 },
 {
     name: `Haunted Museum`,
@@ -69,10 +79,12 @@ link: 'https://walk-ons.com/'
     description: `
     Haunted museum & Voodoo spirit shop in a documented 200 year old French Quarter Haunted House! Museum tours by day, Ghost Hunts by night!`
     ,
+    location:`826 & 828 N. Rampart Street Enter In On Spirt Gift Shop Side, New Orleans, LA 70116`,
     hours:`Fri-Satu 12pm - 6pm <br>Sund-Thur 12pm - 5pm`,
-        link: 'https://www.tripadvisor.com/Attraction_Review-g60864-d12895811-Reviews-Haunted_Museum-New_Orleans_Louisiana.html'
-    },
+    link: 'https://www.tripadvisor.com/Attraction_Review-g60864-d12895811-Reviews-Haunted_Museum-New_Orleans_Louisiana.html'
+},
 ]
+
 
 const popUp = document.querySelector('.popup')
 
@@ -95,6 +107,10 @@ function displayCardPopUp(index) {
     <div>${sponsers[index].description}</div>
     <br>
     <p></p>
+    <h4 style="text-align:center">Location</h4>
+    <hr>
+    <div>${sponsers[index].location}</div>
+    <br>
     <h4 style="text-align:center">HOURS</h4>
     <hr>
     <div>${sponsers[index].hours}</div>
